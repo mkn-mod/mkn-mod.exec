ce7c6c68edf6f54825ef0f73dbbd92f6d59cdfa6
dekken <philip.deegan@gmail.com>
2017-03-14 22:24:30 +0000

    add namespace to avoid global collisions with other mods

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,33 @@
+/**
+Copyright (c) 2013, Philip Deegan.
+All rights reserved.
 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+    * Neither the name of Philip Deegan nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include <maiken.hpp>
 #define KUL_EXPORT
@@ -13,6 +42,7 @@
 #define _MKN_MOD_EXEC_EMPTY_STRING "Warning: empty string provided to mkn-mod.exec"
 #endif//_MKN_MOD_EXEC_EMPTY_STRING
 
+namespace mkn { namespace mod { namespace exec {
 class MaikenModule : public maiken::Module{
     private:
         bool check(const YAML::Node& node) throw (std::exception){
@@ -43,11 +73,12 @@ class MaikenModule : public maiken::Module{
             if(check(node)) exec(node.Scalar());
         }
 };
+}}}
 
 extern "C" 
 KUL_PUBLISH 
 maiken::Module* maiken_module_construct() {
-    return new MaikenModule;
+    return new mkn :: mod :: exec :: MaikenModule;
 }
 
 extern "C" 
